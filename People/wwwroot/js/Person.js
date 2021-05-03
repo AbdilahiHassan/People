de9867946d53414cb31ee1dddf3d8cbbce94a014
Abdilahi khaliif Hassan <daarood11@hotmail.com>
2021-05-03 14:00:22 +0200

    Update

--- People/wwwroot/js/Person.js
+++ People/wwwroot/js/Person.js
@@ -1,40 +1,45 @@
-﻿
+﻿"use strict";
+
 
     //Delaration and getting page info by ID
     var AllPeople = document.getElementById("allpeople");
     var PeopleBtn = document.getElementById("PeopleBtn");
     var DetailInputElm = document.getElementById("textinput");
-    var DeleteBtn = document.getElementById("DeleteBtn");
+var Closepersondetails = document.getElementById("Closepersondetails");
     //ResultdIV
     var resultDiv = document.getElementById("ResultDiv");
     //EventLitsnars
     AllPeople.addEventListener("click", AjaxGetpeople);
-    PeopleBtn.addEventListener("click", axajPostperson);
+PeopleBtn.addEventListener("click", axajPostperson);
+
+ //DetailInputElm.addEventListen("click", AjaxPersonDetails);
+//onclic
     //DeleteBtn.addEventListener("click", axajGetperson);//Onclic
-    //DetailInputElm.addEventListen("click", AjaxPersonDetails);//onclic
+    
 
     //Fetching all data
-    function AjaxGetpeople(event) {
+function AjaxGetpeople(event) {
+    console.log("OnSubmit Event: ", event);
         $.get("Ajax/PeoplePartialView", function (data, status) {
             console.log("Data: " + data + "\nStatus: " + status);
             resultDiv.innerHTML = data;
         });
     }
     // Posting data
-    function axajPostperson(event) {
-        $.post("Ajax/DetailsPartialView",
+function axajPostperson(event, id) {
+    $.post("Ajax/DetailsPartialView",
 
-            {
-                id: DetailInputElm.value
-            }
-            , function (data, status) {
-                console.log("Data: " + data + "\nStatus: " + status);
-                resultDiv.innerHTML = data;
-            });
-    }
+        {
+            id: DetailInputElm.value
+        },
+        function (data, status) {
+
+            resultDiv.innerHTML = data;
+        });
+}
 
 
-    function AjaxPersonDetails(event, id) {
+    function AjaxPersonDetails(event,id) {
         event.preventDefault();
         console.log("OnDetials Event: ", event);
 
@@ -53,6 +58,7 @@
                 $("#person" + id).replaceWith(data);
             }
         );
+        
     }
 
     function Closepersondeials(event, id) {
@@ -77,5 +83,5 @@
         );
 
     }
-}
+
 
